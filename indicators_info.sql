94fb23c81c6e2dfe45a60038bc2cb57fc5e53835
Laura Fontanills <lfontanills@gmail.com>
2023-02-22 15:00:46 -0500

    Changed file name

---
+++ indicators_info.sql
@@ -0,0 +1,30 @@
+-- Get list of indicator topics
+SELECT 
+  topic,
+  COUNT(topic)
+FROM bigquery-public-data.world_bank_intl_education.series_summary
+GROUP BY topic;
+
+-- Get list of indicators for topic: Expenditures, Primary
+SELECT
+  *
+FROM bigquery-public-data.world_bank_intl_education.series_summary
+WHERE
+  (topic IN ('Expenditures')
+    AND indicator_name LIKE '%primary%')
+  OR topic IN ('Primary');
+
+-- INDICATORS AND CODES - EXPENDITURES
+-- UIS.XGDP.1.FSGOV -- Government expenditure on primary education as % of GDP (%)
+
+SELECT 
+  series_code,
+  topic,
+  indicator_name,
+  long_definition
+FROM 
+  `bigquery-public-data.world_bank_intl_education.series_summary`
+WHERE 
+  series_code IN ('UIS.XGDP.1.FSGOV')
+ORDER BY 
+  series_code;
